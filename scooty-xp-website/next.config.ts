1d87418f88ab9a8886e8f7a1bd462b755b1b56a6
devops-scooty <dev@myscooty.ng>
2026-05-01 17:55:53 +0000

    test_3

--- scooty-xp-website/next.config.ts
+++ scooty-xp-website/next.config.ts
@@ -4,11 +4,14 @@ const nextConfig: NextConfig = {
     // output: "standalone",
     output: "export",
   // Optional: If deploying to a subdirectory like [https://username.github.io/scooty-xp-website/](https://username.github.io/scooty-xp-website/)
-  basePath: "/scooty-xp-website",
+    basePath: "/scooty-xp-website",
+  allowedDevOrigins: ['127.0.0.1'],
   images: {
     unoptimized: true, // Image optimization API is unsupported on GitHub Pages
   }
     
 };
-
+module.exports = {
+  allowedDevOrigins: ['127.0.0.1'],
+}
 export default nextConfig;
