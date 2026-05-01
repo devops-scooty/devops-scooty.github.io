6e2d7ce1c1f4c44e274c517ed544e68dfc7a1076
devops-scooty <dev@myscooty.ng>
2026-05-01 23:56:41 +0000

    test_9

--- scooty-xp-website/next.config.ts
+++ scooty-xp-website/next.config.ts
@@ -1,17 +1,8 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  // Force the build to deploy successfully by bypassing strict linters
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-} as any; // Cast to 'any' to bypass strict TS literal checks for 'eslint'
+};
 
 export default nextConfig;
